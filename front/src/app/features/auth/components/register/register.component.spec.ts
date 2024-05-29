06871f325583e57775fe7592ae90ec58247748fd
POP's Code Lab <pop.code.lab13@gmail.com>
2024-05-29 21:12:28 +0700

    Register component unit tests

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,18 +9,39 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
 
 import { RegisterComponent } from './register.component';
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
+import {of, throwError} from "rxjs";
+
+import { jest } from '@jest/globals';
 
 describe('RegisterComponent', () => {
-  let component: RegisterComponent;
+  let registerComponent: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+
+  // Mocks
+
+  let mockAuthService =  {
+    register: jest.fn()
+  }
+
+  let mockRoute = {
+    navigate : jest.fn()
+  }
+
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService},
+        { provide : Router, useValue : mockRoute}
+      ],
       imports: [
         BrowserAnimationsModule,
         HttpClientModule,
-        ReactiveFormsModule,  
+        ReactiveFormsModule,
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
@@ -30,11 +51,45 @@ describe('RegisterComponent', () => {
       .compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
-    component = fixture.componentInstance;
+    registerComponent = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(registerComponent).toBeTruthy();
   });
+
+  describe('submit', () => {
+    it('should register a new user and then navigate to login page', () => {
+      mockAuthService.register.mockReturnValue(of(fixture))
+
+      registerComponent.submit()
+
+      expect(mockRoute.navigate).toHaveBeenCalledWith(['/login'])
+      expect(registerComponent.onError).toBe(false)
+    })
+
+    test('if it display an error message', () => {
+      const ERROR_MESSAGE='An error occurred';
+      mockAuthService.register.mockImplementation(() => {
+        return throwError(ERROR_MESSAGE);
+      })
+
+      registerComponent.submit()
+
+      expect(registerComponent.onError).toBe(true)
+    })
+
+    // Errors
+    it('should display an error message when an error occurs', () => {
+      registerComponent.onError = true;
+      fixture.detectChanges();
+      const formElement: HTMLElement = fixture.nativeElement;
+      const errorMessage: Element | null = formElement.querySelector('span.error');
+      expect(errorMessage).toBeTruthy();
+      expect(errorMessage!.textContent).toContain('An error occurred');
+    });
+
+  })
+
 });
