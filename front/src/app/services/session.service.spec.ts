d824f7b8ea9a88083b553808020895ea946124a7
POP's Code Lab <pop.code.lab13@gmail.com>
2024-06-04 11:53:33 +0700

    SessionService & TeacherService unit tests

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -2,16 +2,53 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionService } from './session.service';
+import {SessionInformation} from "../interfaces/sessionInformation.interface";
 
 describe('SessionService', () => {
   let service: SessionService;
+  const sessionInformationMock : SessionInformation  = {
+    token: 'myDummyToken',
+    type: 'course',
+    id: 1,
+    username: 'john.doe@mail.com',
+    firstName: 'John',
+    lastName: 'Doe',
+    admin: false
+  };;
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(SessionService);
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('logIn', () => {
+    beforeEach(() => {
+      service.logIn(sessionInformationMock)
+    })
+    it('should set session information',() => {
+      expect(service.sessionInformation).toEqual(sessionInformationMock)
+    })
+    it('should set isLogged to true', () => {
+      expect(service.isLogged).toBe(true)
+    })
+  })
+
+  describe('logOut', () => {
+    beforeEach(() => {
+      service.logOut()
+    })
+    it('should set sessionInformation to undefined', () => {
+      expect(service.sessionInformation).toBe(undefined)
+    })
+    it('should set isLogged to false', () => {
+      expect(service.isLogged).toBe(false)
+    })
+  })
+
 });
--- front/src/app/services/teacher.service.spec.ts
+++ front/src/app/services/teacher.service.spec.ts
@@ -1,16 +1,24 @@
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
+import { jest } from '@jest/globals';
 
 import { TeacherService } from './teacher.service';
+import {of} from "rxjs";
 
 describe('TeacherService', () => {
   let service: TeacherService;
+  let httpClientMock = {
+    get : jest.fn()
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports:[
         HttpClientModule
+      ],
+      providers : [
+        {provide : HttpClient, useValue : httpClientMock}
       ]
     });
     service = TestBed.inject(TeacherService);
@@ -19,4 +27,12 @@ describe('TeacherService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get teacher details by id', () => {
+    httpClientMock.get.mockReturnValue(of(null));
+    service.detail('1').subscribe(teacher => {
+      expect(teacher).toEqual(null);
+    });
+    expect(httpClientMock.get).toHaveBeenCalledWith('api/teacher/1');
+  })
 });
