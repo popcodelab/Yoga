dcad6c0da788a4e10c5ea7282b3459a44d3a19d8
POP's Code Lab <pop.code.lab13@gmail.com>
2024-06-09 09:16:29 +0700

    Back-end : UserDetailsImplTest unit tests

---
+++ back/src/test/java/com/openclassrooms/starterjwt/security/services/UserDetailsImplTest.java
@@ -0,0 +1,59 @@
+package com.openclassrooms.starterjwt.security.services;
+
+import com.openclassrooms.starterjwt.repository.SessionRepository;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import com.openclassrooms.starterjwt.services.SessionService;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Collection;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+@Log4j2
+public class UserDetailsImplTest {
+    private static Instant startedAt;
+
+
+    @BeforeAll
+    public static void initializeTestStartTime() {
+        startedAt = Instant.now();
+        log.info("Starts tests at {}", startedAt);
+    }
+
+    @AfterAll
+    public static void displayTestDuration() {
+        Instant endedAt = Instant.now();
+        log.info("Test duration : {} ms", Duration.between(startedAt, endedAt).toMillis());
+    }
+
+    @Tag("equals")
+    @Test
+    @DisplayName("Test if equals returns true when user ID are the same")
+    public void equals_withSameId_shouldReturnTrue() {
+        // Arrange
+        UserDetailsImpl userDetailsImpl1 = UserDetailsImpl.builder().id(1L).build();
+        UserDetailsImpl userDetailsImpl2 = UserDetailsImpl.builder().id(1L).build();
+
+        // Assert
+        assertThat(userDetailsImpl1.equals(userDetailsImpl2)).isTrue();
+    }
+
+    @Tag("equals")
+    @Test
+    @DisplayName("Test if equals returns false  when user ID are not the same")
+    public void equals_withDifferentId_shouldReturnFalse() {
+        UserDetailsImpl userDetailsImpl1 = UserDetailsImpl.builder().id(1L).build();
+        UserDetailsImpl userDetailsImpl2 = UserDetailsImpl.builder().id(2L).build();
+
+        assertThat(userDetailsImpl1.equals(userDetailsImpl2)).isFalse();
+    }
+
+
+}
