afea54f037ff26a937a2285dbfb8830bca964c5c
POP's Code Lab <pop.code.lab13@gmail.com>
2024-06-09 13:47:55 +0700

    Back-end : LoginRequestTest unit tests around constraints

--- back/src/test/java/com/openclassrooms/starterjwt/payload/request/LoginRequestTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/payload/request/LoginRequestTest.java
@@ -1,15 +1,19 @@
 package com.openclassrooms.starterjwt.payload.request;
 
 import lombok.extern.log4j.Log4j2;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Duration;
 import java.time.Instant;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Log4j2
 public class LoginRequestTest {
@@ -27,17 +31,23 @@ public class LoginRequestTest {
         log.info("Test duration : {} ms", Duration.between(startedAt, endedAt).toMillis());
     }
 
+    private Validator validator;
+
+   @BeforeEach
+   public void setup(){
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
 
     @Test
     @DisplayName("Getters and setters test")
     public void testGettersAndSetters() {
-        // Arrange
+       // Arrange
         String email = "john.doe@mail.com";
         String password = "password";
 
-        LoginRequest loginRequest = new LoginRequest();
-
         // Act
+        LoginRequest loginRequest = new LoginRequest();
         loginRequest.setEmail(email);
         loginRequest.setPassword(password);
 
@@ -46,6 +56,30 @@ public class LoginRequestTest {
         assertEquals(password, loginRequest.getPassword());
     }
 
+    @Test
+    @DisplayName("Email cannot be empty")
+    public void testEmailNotEmptyConstraint() {
+       // Arrange & Act
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail("");
+        loginRequest.setPassword("password");
 
+        // Assert
+        Set<ConstraintViolation<LoginRequest>> violations = validator.validate(loginRequest);
+        assertTrue(violations.stream().anyMatch(v -> v.getPropertyPath().toString().equals("email")));
+    }
+
+    @Test
+    @DisplayName("Password cannot be empty")
+    public void testPasswordNotEmptyConstraint() {
+        // Arrange & Act
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail("john.doe@mail.com");
+        loginRequest.setPassword("");
+
+        // Assert
+        Set<ConstraintViolation<LoginRequest>> violations = validator.validate(loginRequest);
+        assertTrue(violations.stream().anyMatch(v -> v.getPropertyPath().toString().equals("password")));
+    }
 
 }
