95dbc9824cd6baea196218c3067f2b6a4a12eed5
POP's Code Lab <pop.code.lab13@gmail.com>
2024-06-09 13:31:56 +0700

    Back-end : JwtResponseTest unit test

---
+++ back/src/test/java/com/openclassrooms/starterjwt/payload/response/JwtResponseTest.java
@@ -0,0 +1,55 @@
+package com.openclassrooms.starterjwt.payload.response;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Log4j2
+public class JwtResponseTest {
+    private static Instant startedAt;
+
+
+    @BeforeAll
+    public static void initializeTestStartTime() {
+        startedAt = Instant.now();
+        log.info("Starts tests at {}", startedAt);
+    }
+
+    @AfterAll
+    public static void displayTestDuration() {
+        Instant endedAt = Instant.now();
+        log.info("Test duration : {} ms", Duration.between(startedAt, endedAt).toMillis());
+    }
+
+
+
+    @Test
+    @DisplayName("Test constructor with all arguments")
+    public void constructor_AllArgs() {
+        String token = "testToken";
+        Long id = 1L;
+        String username = "testUser";
+        String firstName = "John";
+        String lastName = "Doe";
+        Boolean admin = true;
+
+        JwtResponse jwtResponse = new JwtResponse(token, id, username, firstName, lastName, admin);
+
+        assertEquals(token, jwtResponse.getToken());
+        assertEquals("Bearer", jwtResponse.getType());
+        assertEquals(id, jwtResponse.getId());
+        assertEquals(username, jwtResponse.getUsername());
+        assertEquals(firstName, jwtResponse.getFirstName());
+        assertEquals(lastName, jwtResponse.getLastName());
+        assertEquals(admin, jwtResponse.getAdmin());
+    }
+
+
+}
